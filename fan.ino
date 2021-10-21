ae95fdc44526c6118d8fddfea69d1104c0e17c5b
student <rahmafaizyusuf@gmail.com>
2021-10-21 12:14:53 +0300

    code development for fan

---
+++ fan.ino
@@ -0,0 +1,7 @@
+// the loop function runs over and over again forever
+void fan() {
+  digitalWrite(RELAY_PIN, HIGH); // turn on fan 5 seconds
+  delay(5000);
+  digitalWrite(RELAY_PIN, LOW);  // turn off fan 5 seconds
+  delay(5000);
+}
